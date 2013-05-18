573dab233d3521236a5fa21c4db95477079a4948
dmitry <d.demidov@itransition.com>
2013-05-18 06:43:27 +0300

    Empty django project is created

---
+++ setup.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python
+ 
+from setuptools import setup, find_packages
+ 
+
+ 
+setup(
+  name="curs",
+  version="1.0",
+  description="Recommendation system",
+  author="demidov",
+  packages = find_packages('curs'),
+  package_dir = {'': 'curs'},
+  include_package_data = True,
+  zip_safe = False,
+  install_requires=[
+    'django == 1.5.1',
+    'neomodel',
+  ]
+)
